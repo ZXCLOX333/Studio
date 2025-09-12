6e476dc9b4be269abaccd76829d989ea7ce5fb34
ZXCLOX333 <sashakostyrko8@gmail.com>
2025-09-12 14:07:58 +0300

    Меню

--- client/pages/Index.tsx
+++ client/pages/Index.tsx
@@ -1422,7 +1422,7 @@ export default function Index() {
                     placeholder="Ваш відгук"
                     required
                     style={{
-                      minHeight: 120,
+                      minHeight: 180,
                       marginTop: 0,
                       marginBottom: 8,
                     }}
